9c8f6456f174c96729e6e0ca6c3b1116df772082
Shuri <165612083+VibraniumS@users.noreply.github.com>
2024-12-03 13:47:46 -0500

    named getComputerChoice as choice variable so we can use it in the if/else if statements

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,15 @@
 
-function cchoice() {
-    return Math.floor(Math.random() * 10 + 1);
+function getComputerChoice() {
+    return Math.floor(Math.random() * 12 + 1);
 }
 
-if (cchoice()<= 4); {
-    console.log("rock");
-}
+const choice = getComputerChoice();
 
+if (choice >= 9) {
+    console.log("rock"); 
+}   else if  (choice >= 5) {
+    console.log("paper");
+}   else { 
+    console.log("scissors");
+}
 
